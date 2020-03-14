077291fb65d1b259c6c5436a69b7dde98d9559d4
Dongqing Ye <dongqingye7@gmail.com>
2020-03-14 16:54:47 -0500

    Updates

---
+++ precache-manifest.39d4e390155eb1ef5014dbe5b1a74067.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4c97461b8619fdbb9b733353c4c437",
+    "url": "/COVID-19-Tracking-System/master/index.html"
+  },
+  {
+    "revision": "6e594375e49fc5ad7b3f",
+    "url": "/COVID-19-Tracking-System/master/static/css/2.c85ac9d1.chunk.css"
+  },
+  {
+    "revision": "4c76777768e930e0e38e",
+    "url": "/COVID-19-Tracking-System/master/static/css/main.0a7131b5.chunk.css"
+  },
+  {
+    "revision": "6e594375e49fc5ad7b3f",
+    "url": "/COVID-19-Tracking-System/master/static/js/2.7c04c3f1.chunk.js"
+  },
+  {
+    "revision": "4c76777768e930e0e38e",
+    "url": "/COVID-19-Tracking-System/master/static/js/main.af744af5.chunk.js"
+  },
+  {
+    "revision": "c4e2d929078ba57344ac",
+    "url": "/COVID-19-Tracking-System/master/static/js/runtime~main.7ad63bb1.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/COVID-19-Tracking-System/master/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/COVID-19-Tracking-System/master/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/COVID-19-Tracking-System/master/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/COVID-19-Tracking-System/master/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/COVID-19-Tracking-System/master/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/COVID-19-Tracking-System/master/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/COVID-19-Tracking-System/master/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/COVID-19-Tracking-System/master/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/COVID-19-Tracking-System/master/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/COVID-19-Tracking-System/master/static/media/sidebar-2.7a4ce7cc.jpg"
+  }
+]);
