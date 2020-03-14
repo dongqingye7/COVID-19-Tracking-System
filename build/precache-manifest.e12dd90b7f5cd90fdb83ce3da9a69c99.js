cff5d37895e19de7b061c36394400ba793376e7a
Dongqing Ye <dongqingye7@gmail.com>
2020-03-14 16:30:12 -0500

    fix issues in assets

---
+++ build/precache-manifest.e12dd90b7f5cd90fdb83ce3da9a69c99.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5032e5fb7ccce53aee0af452557e77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14b4f4bcaaa11b1f4a88",
+    "url": "/static/css/2.a4d11b28.chunk.css"
+  },
+  {
+    "revision": "9f4698596237860049d2",
+    "url": "/static/css/main.8378a6c5.chunk.css"
+  },
+  {
+    "revision": "14b4f4bcaaa11b1f4a88",
+    "url": "/static/js/2.7c04c3f1.chunk.js"
+  },
+  {
+    "revision": "9f4698596237860049d2",
+    "url": "/static/js/main.af744af5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/static/media/sidebar-2.7a4ce7cc.jpg"
+  }
+]);
